1666d533e347c95527ff9d9259f99690c7fed9f4
Luísa Machado <luisamachadossss@gmail.com>
2022-11-09 19:58:52 -0300

    Foi criada a relação com o banco de dados

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using api1.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace api1.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt)
+        {
+
+        }
+        public DbSet<Filmes> Filmes { get; set; }
+    }
+}
---
+++ Models/Filmes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api1.Modelos
+{
+    public class Filmes
+    {
+        public int Id { get; internal set; }
+        [Required (ErrorMessage =  "O campo não pode estar vazio")]
+        public string Titulo {get; set;}
+        [Required(ErrorMessage = "O campo não pode estar vazio")]
+        public string Diretor {get; set;}
+        public string Genero {get; set;}
+        [Range(1,80, ErrorMessage =  "O valor de tempo não é válido")]
+        public int Duracao {get; set;}
+    
+    }
+}
